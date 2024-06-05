d4defab71935272bd5c339120a0ef5571ac3ee34
Je <jemacieldev@gmail.com>
2024-06-05 14:02:09 -0300

    ajustes

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,47 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const imagemin = require('gulp-imagemin');
-const uglify = require('gulp-uglify');
-const rename = require('gulp-rename');
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const imagemin = require("gulp-imagemin");
+const uglify = require("gulp-uglify");
+const rename = require("gulp-rename");
 
 // Caminhos
 const paths = {
   styles: {
-    src: 'src/scss/**/*.scss',
-    dest: 'dist/css'
+    src: "src/scss/**/*.scss",
+    dest: "dist/css",
   },
   scripts: {
-    src: 'src/js/**/*.js',
-    dest: 'dist/js'
+    src: "src/js/**/*.js",
+    dest: "dist/js",
   },
   images: {
-    src: 'src/images/**/*',
-    dest: 'dist/images'
-  }
+    src: "src/images/**/*",
+    dest: "dist/images",
+  },
 };
 
 // Compilar SASS
 function styles() {
-  return gulp.src(paths.styles.src)
-    .pipe(sass().on('error', sass.logError))
+  return gulp
+    .src(paths.styles.src)
+    .pipe(sass().on("error", sass.logError))
     .pipe(gulp.dest(paths.styles.dest));
 }
 
 // Compressão de Imagens
 function images() {
-  return gulp.src(paths.images.src)
+  return gulp
+    .src(paths.images.src)
     .pipe(imagemin())
     .pipe(gulp.dest(paths.images.dest));
 }
 
 // Compressão de JavaScript
 function scripts() {
-  return gulp.src(paths.scripts.src)
+  return gulp
+    .src(paths.scripts.src)
     .pipe(uglify())
-    .pipe(rename({ extname: '.min.js' }))
+    .pipe(rename({ extname: ".min.js" }))
     .pipe(gulp.dest(paths.scripts.dest));
 }
 
